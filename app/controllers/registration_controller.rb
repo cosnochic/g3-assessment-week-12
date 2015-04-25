dfbc99481b23bd609ab17f85bfd40c734410bfe1
Melissa Khat <melissa.khat@gmail.com>
2015-04-25 17:44:42 -0600

    user can sign up

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -8,8 +8,14 @@ class RegistrationController < ApplicationController
       if @user.save
          redirect_to '/movies'
       else
-         redirect_to '/signup', notice: "You must be signed in."
+         redirect_to '/signup', notice: "Sign up to view movies and reviews."
       end
    end
-   
+
+private
+def user_params
+   params.require(:user).permit(:username, :password, :password_confirmation)
+end
+
+
 end
