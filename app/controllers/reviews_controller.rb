29e9f0b0204f4e35031c5f9626eba55679334c97
Melissa Khat <melissa.khat@gmail.com>
2015-04-25 17:38:39 -0600

    crud reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+   before_action :authenticate
+
+   def index
+      @reviews = Review.all
+   end
+
+   def new
+      @review = Review.new
+   end
+
+   def create
+      @review = Review.new(review_params)
+      if @review.save
+         redirect_to reviews_path, notice: "You've successfully created a review!"
+      else
+         render :new
+      end
+   end
+
+
+   private
+   def review_params
+      params.require(:review).permit(:movie_id, :user_id, :title, :body)
+end
