2256cfdd51f2ad837dab4282a368eace5b2ba1b8
Melissa Khat <melissa.khat@gmail.com>
2015-04-25 17:15:09 -0600

    add controllers and models for movies, reviews, user, session, and registration

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,23 @@
+class SessionController < ApplicationController
+
+
+   def new
+   end
+
+   def create
+      @user = User.find(params[:username])
+      if user && user.authenticate(params[:password])
+         session[:user_id] = @user.id
+         redirect_to movies_path
+      else
+         render '/signin'
+      end
+   end
+
+
+   def destroy
+      session.destroy
+      session[:user_id] = nil
+      redirect_to '/signin', notice: "You are now logged out."
+   end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+   validates :username, :password, presence: true
+
+   has_secure_password
+
+   has_many :reviews
+
+end
