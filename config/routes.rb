e2f23c0db84b081b057814f0c0e12327bc21e4ae
Melissa Khat <melissa.khat@gmail.com>
2015-04-27 17:51:33 -0600

    add 2nd reviews route; add links to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'reviews/index'
+
+  get 'reviews/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,6 +15,7 @@ Rails.application.routes.draw do
   end
 
   resources :users
+  resources :reviews
 
   get '/signin' => 'session#new'
   post '/signin' => 'session#create'
